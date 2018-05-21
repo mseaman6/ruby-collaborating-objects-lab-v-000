31dcd7fef9df18a2569b667db0fb7ca194a0020b
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-21 18:10:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,7 @@ class Song
   end
 
   def self.new_by_filename(file_name)
-    song = self.new
-    song.name = file_name.split(" - ")[1]
+    song = self.new(file_name.split(" - ")[1])
     song.artist = file_name.split(" - ")[0]
     self.artist_name(artist_name)
     Artist.add_song(song)
