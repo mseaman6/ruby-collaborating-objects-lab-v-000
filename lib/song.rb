96f559ceca9edad6b181176eb9f135f3dac8beef
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-21 18:27:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,14 +6,14 @@ class Song
     @name = name
   end
 
-  def self.artist_name=(artist_name)
-    Artist.find_or_create_by_name(artist_name)
-  end
+#  def self.artist_name=(artist_name)
+#    Artist.find_or_create_by_name(artist_name)
+#  end
 
   def self.new_by_filename(file_name)
     song = self.new(file_name.split(" - ")[1])
     artist = file_name.split(" - ")[0]
-    self.artist_name = artist
+    Artist.find_or_create_by_name(artist)
   end
 
 end
